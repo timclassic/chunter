e75597d4536eec1ffe2ddb6484c6bbc7dffe2354
Heinz N. Gies <heinz@licenser.net>
2012-12-04 03:42:16 +0100

    Added version check.

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -2,17 +2,33 @@
 
 . /usbkey/config
 
+TESTED_VERSIONS=joyent_20120906T221231Z
+
 DST=/opt
 
 IFACE=`dladm show-phys -m | grep $admin_nic | awk -e '{print $1}'`
 IP=`ifconfig $IFACE | grep inet | awk -e '{print $2}'`
 
 DIR=`dirname $0`;
-if [ x$DIR = "x." ]
+if [[ "x$DIR" = "x." ]]
 then
     DIR=`pwd`
 fi
 BASE=`basename $0`;
+
+if uname -a | grep $TESTED_VERSIONS
+then
+    echo "This SnartOS release is tested!"
+else
+    read  "This SnartOS release WAS NOT tested! Are you sure you want to go on? [yes|NO] " SKIP
+    if [[ "$SKIP" = "yes" ]]
+    then
+	echo "Okay we go on, but it mit not work!"
+    else
+	echo "Exiting."
+	exit 1
+    fi
+fi
 
 (cd $DST; uudecode -p $DIR/$BASE|tar xzf -)
 mkdir -p /var/log/chunter

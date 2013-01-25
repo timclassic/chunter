b11b9cc8863e794b8e3bb764d903f66418b45d58
Heinz N. Gies <heinz@licenser.net>
2013-01-25 23:52:49 +0100

    Fixed some more console bugs.

--- apps/chunter/src/chunter_vm_fsm.erl
+++ apps/chunter/src/chunter_vm_fsm.erl
@@ -370,7 +370,7 @@ handle_event(delete, StateName, State) ->
 handle_event({console, send, _Data}, StateName, State = #state{write = undefined}) ->
     {next_state, StateName, State};
 
-handle_event({console, link, _Pid}, StateName, State = #state{write = undefined}) ->
+handle_event({console, link, _Pid}, StateName, State = #state{read = undefined}) ->
     {next_state, StateName, State};
 
 handle_event({console, send, Data}, StateName, State) ->
@@ -378,7 +378,7 @@ handle_event({console, send, Data}, StateName, State) ->
     {next_state, StateName, State};
 
 handle_event({console, link, Pid}, StateName, State) ->
-    port_connect(State#state.write, Pid),
+    port_connect(State#state.read, Pid),
     {next_state, StateName, State};
 
 handle_event(_Event, StateName, State) ->
@@ -461,7 +461,7 @@ code_change(_OldVsn, StateName, State, _Extra) ->
 %%%===================================================================
 
 
-init_console(State) ->
+init_console(State = #state{write = undefined, read = undefined}) ->
     [{_,Name,_, Path, _, _}] = zoneadm(State#state.uuid),
     Base = binary_to_list(Path) ++ "/fifo",
     RPath = Base ++ ".r",
@@ -474,8 +474,13 @@ init_console(State) ->
             os:cmd("/opt/chunter/erts-5.9.1/bin/run_erl "++ Base ++
                        " /tmp \"/usr/sbin/zlogin -C "++ binary_to_list(Name) ++ "\"")
     end,
-    Write = open_port({spawn, "/opt/chunter/erts-5.9.1/bin/to_erl " ++ Base}, [binary]),
-    State#state{write = Write}.
+    {ok, Write} = open_port({spawn,"/bin/cat > " ++ WPath}, [binary, in, eof]),
+    {ok, Read} = open_port({spawn,"/bin/cat " ++ WPath}, [binary, out, eof]),
+    State#state{read = Read, write = Write};
+
+init_console(State) ->
+    State.
+
 
 -spec install_image(DatasetUUID::fifo:uuid()) -> ok | string().
 

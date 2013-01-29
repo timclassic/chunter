e0aadca680b4f911c32e6784b8aed3e4fec2db31
Heinz N. Gies <heinz@licenser.net>
2013-01-29 01:41:53 +0100

    Fixed terminate.

--- apps/chunter/src/chunter_vm_fsm.erl
+++ apps/chunter/src/chunter_vm_fsm.erl
@@ -449,7 +449,7 @@ terminate(_Reason, _StateName, State  = #state{console = _C}) when is_port(_C) -
     port_close(State#state.console),
     ok;
 
-terminate(_Reason, _StateName, State) ->
+terminate(_Reason, _StateName, _State) ->
     ok.
 
 %%--------------------------------------------------------------------

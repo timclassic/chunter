87bd969e738b1361cb06d7d70b5c29b92bc4a205
Heinz N. Gies <heinz@licenser.net>
2013-03-04 14:50:07 +0100

    oops.

--- apps/chunter/src/chunter_protocol.erl
+++ apps/chunter/src/chunter_protocol.erl
@@ -92,7 +92,7 @@ handle_info({_OK, Socket, BinData},  State = #state{
             erltrace:stop(Handle);
         go ->
             erltrace:go(Handle);
-        {Act, Ref, Fn} ->
+        {Act, Ref, _Fn} ->
             lager:info("<~p> Starting ~p.", [Ref, Act]),
             Transport:send(Socket, term_to_binary({ok, Ref})),
             {Time, Res} = timer:tc(fun() ->
@@ -104,7 +104,7 @@ handle_info({_OK, Socket, BinData},  State = #state{
                                            end
                                    end),
             {Time1, Res1} = timer:tc(fun() ->
-                                             %Fn(Res)
+                                                %Fn(Res)
                                              Res
                                      end),
             Now = now(),

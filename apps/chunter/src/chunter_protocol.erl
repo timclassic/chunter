ea91bd24a8ca6404ebc3efb9db41f2e0cc709e45
Heinz N. Gies <heinz@licenser.net>
2013-03-04 22:24:25 +0100

    Adjusted protocol behaviour.

--- apps/chunter/src/chunter_protocol.erl
+++ apps/chunter/src/chunter_protocol.erl
@@ -92,7 +92,7 @@ handle_info({_OK, Socket, BinData},  State = #state{
             erltrace:stop(Handle);
         go ->
             erltrace:go(Handle);
-        {Act, Ref, _Fn} ->
+        {Act, Ref, Fn} ->
             lager:info("<~p> Starting ~p.", [Ref, Act]),
             Transport:send(Socket, term_to_binary({ok, Ref})),
             {Time, Res} = timer:tc(fun() ->
@@ -103,10 +103,13 @@ handle_info({_OK, Socket, BinData},  State = #state{
                                                    erltrace:consume(Handle)
                                            end
                                    end),
-            {Time1, Res1} = timer:tc(fun() ->
-                                                %Fn(Res)
-                                             Res
-                                     end),
+            cas
+                {Time1, Res1} = timer:tc(fun () ->
+                                                 case Fn of 
+                                                     identity ->
+                                                         Res
+                                                 end
+                                         end),
             Now = now(),
             Transport:send(Socket, term_to_binary(Res1)),
             lager:info("<~p> Dtrace ~p  took ~pus + ~pus + ~pus.", [Ref, Act, Time, Time1, timer:now_diff(now(), Now)])

bb9da75258c02af288590a499605bfa50847f96a
Heinz N. Gies <heinz@licenser.net>
2013-03-23 06:03:37 +0100

    Added ekstat.

--- plugins/zone_vfs/zone_vfs.erl
+++ plugins/zone_vfs/zone_vfs.erl
@@ -1,35 +1,24 @@
 -module(zone_vfs).
 
--export([zone_vfs/0]).
+-export([zone_vfs/1]).
 
-zone_vfs() ->
-    Data = [{Id, Field, to_number(Value)} ||
-               [_, Id,_,Field, Value] <-
-                   [re:split(L, ":|\\t") ||
-                       L <- re:split(os:cmd("kstat  -p -m zone_vfs | grep -v zone_vfs:0:"), "\n")],
-               Field =/= <<"class">>,
-               Field =/= <<"zonename">>],
-    build_obj(Data, zone_map(), []).
+zone_vfs(KStat) ->
+    Data = ekstat:read(KStat, "zone_vfs", "zone_vfs"),
+    Data1 = [{Id, Keys} || {_,_,ID,_, Keys} <- Data, ID <> 0],
+    build_obj(Data, []).
 
-build_obj([{Id, Field, Value} | R], ZoneMap, Data) ->
-    UUID = proplists:get_value(Id, ZoneMap),
+build_obj([{Id, Keys} | R], Data) ->
+    UUID = list_to_binary(proplists:get_value("zonename", Keys)),
+    Statistics = lists:fold(fun({"zonename", _}, Obj) ->
+                                    Obj;
+                               ({K, V}, Obj) ->
+                                    jsxd:set([list_to_binary(Key)], V, Obj)
+                            end, [], Keys),
+
     Data1 = jsxd:thread([{set, [UUID, <<"event">>], <<"vfs">>},
-                         {set, [UUID, <<"data">>, Field], Value}],
+                         {set, [UUID, <<"data">>], Statistics}],
                         Data),
     build_obj(R, ZoneMap, Data1);
 
 build_obj([], _, Data) ->
     Data.
-
-zone_map() ->
-    [_ | R] = re:split(os:cmd("zoneadm list -p"), "\n"),
-    [{Id, UUID} || [Id, UUID | _] <- [re:split(L, ":") || L <- R, L =/= <<>>]].
-
-
-to_number(N) ->
-    case re:run(N, "\\.") of
-        nomatch ->
-            list_to_integer(binary_to_list(N));
-        _ ->
-            list_to_float(binary_to_list(N))
-    end.

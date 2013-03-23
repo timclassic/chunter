78bb7c9d01f798ea066243fb3bcebef35244d943
Heinz N. Gies <heinz@licenser.net>
2013-03-23 06:12:28 +0100

    Fixed plugin.

--- plugins/zone_vfs/zone_vfs.erl
+++ plugins/zone_vfs/zone_vfs.erl
@@ -4,21 +4,20 @@
 
 zone_vfs(KStat) ->
     Data = ekstat:read(KStat, "zone_vfs", "zone_vfs"),
-    Data1 = [{Id, Keys} || {_,_,ID,_, Keys} <- Data, ID <> 0],
-    build_obj(Data, []).
+    Data1 = [Keys || {_,_,_ID,_, Keys} <- Data, _ID =/= 0],
+    build_obj(Data1, []).
 
-build_obj([{Id, Keys} | R], Data) ->
+build_obj([Keys | R], Data) ->
     UUID = list_to_binary(proplists:get_value("zonename", Keys)),
-    Statistics = lists:fold(fun({"zonename", _}, Obj) ->
-                                    Obj;
-                               ({K, V}, Obj) ->
-                                    jsxd:set([list_to_binary(Key)], V, Obj)
-                            end, [], Keys),
-
+    Statistics = lists:foldl(fun({"zonename", _}, Obj) ->
+                                     Obj;
+                                ({K, V}, Obj) ->
+                                     jsxd:set([list_to_binary(K)], V, Obj)
+                             end, [], Keys),
     Data1 = jsxd:thread([{set, [UUID, <<"event">>], <<"vfs">>},
                          {set, [UUID, <<"data">>], Statistics}],
                         Data),
-    build_obj(R, ZoneMap, Data1);
+    build_obj(R, Data1);
 
-build_obj([], _, Data) ->
+build_obj([], Data) ->
     Data.

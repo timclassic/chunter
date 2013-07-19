c832bf8699879ddabcf6f8c309641ce0d3e2c121
Heinz N. Gies <heinz@licenser.net>
2013-07-19 16:48:22 +0200

    fixed bug in pool monitor.

--- apps/chunter/src/chunter_app.erl
+++ apps/chunter/src/chunter_app.erl
@@ -12,7 +12,7 @@
 start(_StartType, _StartArgs) ->
     {ok, Port} = application:get_env(chunter, port),
     {ok, _} = ranch:start_listener(chunter_server, 1,
-				   ranch_tcp, [{port, Port}], chunter_protocol, []),
+                                   ranch_tcp, [{port, Port}], chunter_protocol, []),
 
     lager:info("chunter:load - start.", []),
     chunter_sup:start_link().

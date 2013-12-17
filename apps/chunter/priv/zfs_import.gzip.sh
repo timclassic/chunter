9b71e97c0d45ba0755489622cf6009d9f239679c
Heinz N. Gies <heinz@licenser.net>
2013-12-17 14:40:22 +0100

    Fixed missing execute flag.

--- apps/chunter/priv/zfs_import.gzip.sh
+++ apps/chunter/priv/zfs_import.gzip.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "`date +'%Y-%m-%d %H:%M:%S'` Importing dataset $1 encoded as gzip"
+echo "`date +'%Y-%m-%d %H:%M:%S'` Importing dataset $1 encoded as gzip" >> /var/log/chunter/zfs.log
 gunzip | zfs receive zones/$1@$2 >> /var/log/chunter/zfs_receive.log 2>> /var/log/chunter/zfs.log

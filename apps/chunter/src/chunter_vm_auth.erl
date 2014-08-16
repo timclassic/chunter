141d4d036096e8a41103ac9ada8e65343b01213a
Heinz N. Gies <heinz@licenser.net>
2014-08-16 18:01:09 -0400

    Reindented, fixed call to lists delete.

--- apps/chunter/src/chunter_vm_auth.erl
+++ apps/chunter/src/chunter_vm_auth.erl
@@ -10,56 +10,57 @@
 
 -behaviour(gen_server).
 -export([start_link/0]).
+-ignore_xref([start_link/0]).
+
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -record(state, {port, doors=[]}).
 
 -define(HEATRBEAT_INTERVAL, 1000). % 2 seconds - zonedoor terminates after 7 sec of no HB. Allow 2 misses
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-   
+
 
 init(_) ->
-  Cmd = code:priv_dir(chunter) ++ "/zonedoor",
-	DoorPort = open_port({spawn_executable, Cmd},
-	                     [{args, []}, use_stdio, {line, 1024}, exit_status]),
-  erlang:send_after(?HEATRBEAT_INTERVAL, self(), heartbeat),
-	{ok, #state{port = DoorPort}}.
-
+    Cmd = code:priv_dir(chunter) ++ "/zonedoor",
+    DoorPort = open_port({spawn_executable, Cmd},
+                         [{args, []}, use_stdio, {line, 1024}, exit_status]),
+    erlang:send_after(?HEATRBEAT_INTERVAL, self(), heartbeat),
+    {ok, #state{port = DoorPort}}.
 
 
 handle_call({verify_zonedoor, UUID}, _, State) ->
     case lists:member(UUID, State#state.doors) of
-      false ->
-        io:format("Request to add zone door: ~s~n", [UUID]),
-        Data = io_lib:format("a~s~n", [UUID]),
-        port_command(State#state.port, Data),
-        State1 = State#state{doors = lists:flatten([UUID, State#state.doors])},
-        {reply, ok, State1};
-      _ ->
-       {reply, ok, State}
+        false ->
+            io:format("Request to add zone door: ~s~n", [UUID]),
+            Data = io_lib:format("a~s~n", [UUID]),
+            port_command(State#state.port, Data),
+            State1 = State#state{doors = lists:flatten([UUID, State#state.doors])},
+            {reply, ok, State1};
+        _ ->
+            {reply, ok, State}
     end;
 handle_call({remove_zonedoor, UUID}, _, State) ->
     case lists:member(UUID, State#state.doors) of
-      true ->
-        io:format("Request to delete zone door: ~s~n", [UUID]),
-        Data = io_lib:format("d~s~n", [UUID]),
-        port_command(State#state.port, Data),
-        State1 = State#state{doors = lists:delete([UUID, State#state.doors])},
-        {reply, ok, State1};
-      _ ->
-        {reply, ok, State}
-      end.
+        true ->
+            io:format("Request to delete zone door: ~s~n", [UUID]),
+            Data = io_lib:format("d~s~n", [UUID]),
+            port_command(State#state.port, Data),
+            State1 = State#state{doors = lists:delete(UUID, State#state.doors)},
+            {reply, ok, State1};
+        _ ->
+            {reply, ok, State}
+    end.
 
 
 
 handle_info(heartbeat, State) ->
-  port_command(State#state.port, "h\n"),
-  erlang:send_after(?HEATRBEAT_INTERVAL, self(), heartbeat),
-  {noreply, State};
+    port_command(State#state.port, "h\n"),
+    erlang:send_after(?HEATRBEAT_INTERVAL, self(), heartbeat),
+    {noreply, State};
 handle_info({Port,{data,{eol,Data}}} , State) ->
-	io:format("Received data: ~s~n", [Data]),
-	auth_credintials(Port, Data),
-	{noreply, State}.
+    io:format("Received data: ~s~n", [Data]),
+    auth_credintials(Port, Data),
+    {noreply, State}.
 
 
 
@@ -83,14 +84,14 @@ auth_credintials(Port, [UUID,KeyID]) ->
             lager:warning("[zonedoor:~s] denied.", [UUID]),
             port_command(Port, "r0\n")
     end;
-auth_credintials(Port, Data) -> 
-	case re:split(Data, " ") of
+auth_credintials(Port, Data) ->
+    case re:split(Data, " ") of
         [UUID,_,_,KeyID] ->
-        	auth_credintials(Port, [UUID,KeyID]),
-        	ok;
-    	 _ ->
-          lager:warning("[zonedoor] recived unknown data: ~p.", [Data]),
-          ok
+            auth_credintials(Port, [UUID,KeyID]),
+            ok;
+        _ ->
+            lager:warning("[zonedoor] recived unknown data: ~p.", [Data]),
+            ok
     end.
 
 
@@ -106,9 +107,9 @@ auth_credintials(Port, Data) ->
 %% @end
 %%--------------------------------------------------------------------
 
-terminate(_, State) -> 
-  incinerate(State#state.port),
-  ok.
+terminate(_, State) ->
+    incinerate(State#state.port),
+    ok.
 
 
 

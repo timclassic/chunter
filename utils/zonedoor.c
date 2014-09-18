d9fe4206e2628f2460f766a674987fbdb27e10ca
Heinz N. Gies <heinz@licenser.net>
2014-09-18 10:10:49 -0400

    Added the option for zdoor to allow for other endpoints.

--- utils/zonedoor.c
+++ utils/zonedoor.c
@@ -37,20 +37,20 @@ int pendingRequest;
 char requestResponse;
 
 
-void addVMDoor(char *zoneID){
-  if (zdoor_open(zdid, zoneID, "_joyent_sshd_key_is_authorized", zoneID, server) < 0){
-          fprintf(stderr, "Error [zonedoor] opening door in zone %s.\r\n", zoneID);
-          fprintf(stderr, "zdoor_open result: %i", zdoor_open(zdid, zoneID, "_joyent_sshd_key_is_authorized", zoneID, server));
-          return;
-        }
-//  printf("ok\n");  //no sure if responce is necessary. right now chunter_vm_auth does not handle it.
-//  fflush(stdout);
+void addVMDoor(char *zoneID, char *doorName){
+  if (zdoor_open(zdid, zoneID, doorName, zoneID, server) < 0){
+    fprintf(stderr, "Error [zonedoor] opening door in zone %s.\r\n", zoneID);
+    fprintf(stderr, "zdoor_open result: %i", zdoor_open(zdid, zoneID, "_joyent_sshd_key_is_authorized", zoneID, server));
+    return;
+  }
+  //  printf("ok\n");  //no sure if responce is necessary. right now chunter_vm_auth does not handle it.
+  //  fflush(stdout);
 }
 
-void deleteVMDoor(char *zoneID){
-  zdoor_close(zdid, zoneID, "_joyent_sshd_key_is_authorized");
- // printf("ok\n");
- // fflush(stdout);
+void deleteVMDoor(char *zoneID, char *doorName){
+  zdoor_close(zdid, zoneID, doorName);
+  // printf("ok\n");
+  // fflush(stdout);
 
 }
 
@@ -61,17 +61,17 @@ zdoor_result_t *server(zdoor_cookie_t *cookie, char *argp, size_t arpg_sz)
   fflush(stdout);
   pendingRequest = 1;
   char deny[] = "0";
-  
+
   result = malloc(sizeof(zdoor_result_t));
-          result->zdr_data = NULL;
-          result->zdr_size = 1;
+  result->zdr_data = NULL;
+  result->zdr_size = 1;
 
   int i = 0;
   while(i<20){
-      if(pendingRequest == 0){
-          result->zdr_data = &requestResponse;
-          return result;
-        }
+    if(pendingRequest == 0){
+      result->zdr_data = &requestResponse;
+      return result;
+    }
     i++;
     nanosleep((struct timespec[]){{0, 100000000}}, NULL);
   }
@@ -82,10 +82,10 @@ zdoor_result_t *server(zdoor_cookie_t *cookie, char *argp, size_t arpg_sz)
 
 
 void sigAlrmHandler(int sig) {
-  exit(0); 
+  exit(0);
 }
 
- 
+
 int
 main(int argc, char *argv[])
 {
@@ -95,34 +95,52 @@ main(int argc, char *argv[])
 
   zdid = zdoor_handle_init();
 
-   while(1){
+  while(1){
 
-    size_t nbytes = 200; 
-    char *input = NULL; 
+    size_t nbytes = 200;
+    char *input = NULL;
+    char *arg2 = NULL;
     getline(&input, &nbytes, stdin);
-
-      switch(input[0])
+    switch(input[0])
       {
-        case 'h':    // heartbeat
-          alarm(HEARTBEAT);
-          break;
-        case 'a':    // add zone door
-          input++[strlen(input)-1]=0;
-          addVMDoor(input);
-          break;
-        case 'd':    // delete zone door
-          input++[strlen(input)-1]=0;
-          deleteVMDoor(input);
-          break;
-        case 'r':   // request response
-          requestResponse = input[1];
-          pendingRequest = 0;
-          break;
-        default:
-          break;
+      case 'h':    // heartbeat
+        alarm(HEARTBEAT);
+        break;
+      case 'a':    // add zone door
+        input++[strlen(input)-1]=0;
+        arg2 = input;
+        while (arg2 != ' ' && arg2 - input < nbytes) {
+          arg2++;
+        };
+        if (arg2 == ' ') {
+          *arg2 = 0;
+          arg2++;
+          // "_joyent_sshd_key_is_authorized"
+          addVMDoor(input, arg2);
+        } else {
+          alert("invalid input");
+        }
+        break;
+      case 'd':    // delete zone door
+        input++[strlen(input)-1]=0;
+        if (arg2 == ' ') {
+          *arg2 = 0;
+          arg2++;
+          // "_joyent_sshd_key_is_authorized"
+          deleteVMDoor(input, arg2);
+        } else {
+          alert("invalid input");
+        }
+        break;
+      case 'r':   // request response
+        requestResponse = input[1];
+        pendingRequest = 0;
+        break;
+      default:
+        break;
       }
 
-      free(input);
+    free(input);
 
-   }
+  }
 }

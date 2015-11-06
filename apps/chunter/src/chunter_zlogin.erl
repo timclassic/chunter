b97549b772eea30783079adfb432cb2bf85552ba
Heinz N. Gies <heinz@licenser.net>
2015-11-06 21:13:12 +0100

    Use remote zlogin server

--- apps/chunter/src/chunter_zlogin.erl
+++ apps/chunter/src/chunter_zlogin.erl
@@ -1,10 +1,14 @@
 -module(chunter_zlogin).
 
+-define(ZLOGIN_NODE, 'fifo_zlogin@127.0.0.1').
 
--export([start/2, subscribe/1, send/2]).
+-export([start/2, stop/1, subscribe/1, send/2]).
 
 start(UUID, Type) ->
-    rpc:call('zlogin_fifo@127.0.0.1', zlogin_fsm, start, [UUID, Type]).
+    rpc:call(?ZLOGIN_NODE, zlogin_fsm, start, [UUID, Type]).
+
+stop(UUID) ->
+    gen_fsm:send_event({global, {zlogin, UUID}}, stop).
 
 send(UUID, Data) ->
     gen_fsm:send_event({global, {zlogin, UUID}}, {send, Data}).

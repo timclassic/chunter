33913fe9f06b51f93965fbed3f3e771c8da04a1d
Heinz N. Gies <heinz@licenser.net>
2016-02-24 19:11:33 -0500

    Fix the cuttlefish mess

--- apps/chunter/test/spec_tests.erl
+++ apps/chunter/test/spec_tests.erl
@@ -5,14 +5,14 @@
 -import(chunter_spec, [to_vmadm/3, to_sniffle/1]).
 
 type_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"alias">>, <<"a">>}, {<<"hostname">>, <<"h">>}, {<<"uuid">>, <<"z">>}]),
     In = apply_defaults(InP, InD, InO),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 autoboot_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"alias">>, <<"a">>}, {<<"hostname">>, <<"h">>},
                           {<<"uuid">>, <<"z">>}, {<<"autoboot">>, false}]),
@@ -21,16 +21,16 @@ autoboot_test() ->
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 disk_driver_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>},
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>},
                           {<<"disk_driver">>, <<"virtio">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 created_at_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In0 = apply_defaults_kvm(InP, InD, InO),
     In = jsxd:set(<<"created_at">>, 123, In0),
@@ -39,15 +39,15 @@ created_at_test() ->
 
 
 nic_driver_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>},
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>},
                           {<<"nic_driver">>, <<"virtio">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 zone_ram_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 1024}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 1024}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults(InP, InD, InO),
@@ -56,50 +56,50 @@ zone_ram_test() ->
     ?assertEqual(In, ordsets:from_list(to_sniffle(VMData1))).
 
 kvm_ram_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 1024}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 1024}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 kvm_cpu_cap1_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}, {<<"cpu_cap">>, 100}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InP = jsxd:from_list([ {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}, {<<"cpu_cap">>, 100}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 kvm_cpu_cap14_test() ->
     InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}, {<<"cpu_cap">>, 140}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO, 140, 2),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 kvm_cpu_cap15_test() ->
     InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}, {<<"cpu_cap">>, 150}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO, 150, 2),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 kvm_cpu_cap2_test() ->
     InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}, {<<"cpu_cap">>, 200}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO, 200, 2),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 kvm_cpu_cap21_test() ->
     InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}, {<<"cpu_cap">>, 210}]),
-    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"uuid">>, <<"d">>}]),
+    InD = jsxd:from_list([{<<"type">>, <<"kvm">>}, {<<"image_size">>, 10}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}]),
     In = apply_defaults_kvm(InP, InD, InO, 210, 3),
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 
 resolver_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>}, {<<"resolvers">>, [<<"8.8.8.8">>]}]),
     In0 = apply_defaults(InP, InD, InO),
@@ -107,7 +107,7 @@ resolver_test() ->
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 ssh_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>},
                           {<<"ssh_keys">>,
@@ -116,7 +116,7 @@ ssh_test() ->
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 passwd_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>},
                           {<<"admin_pw">>, <<"admin">>},
@@ -125,7 +125,7 @@ passwd_test() ->
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 metadata_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10}, {<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>},
                           {<<"admin_pw">>, <<"admin">>},
@@ -134,7 +134,7 @@ metadata_test() ->
     ?assertEqual(In, ordsets:from_list(to_sniffle(to_vmadm(InP, InD, InO)))).
 
 nics_test() ->
-    InP = jsxd:from_list([{<<"uuid">>, <<"p">>}, {<<"quota">>, 10},{<<"ram">>, 0}]),
+    InP = jsxd:from_list([{<<"cpu_cap">>, 100}, {<<"uuid">>, <<"p">>}, {<<"quota">>, 10},{<<"ram">>, 0}]),
     InD = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InD1 = jsxd:from_list([{<<"type">>, <<"zone">>}, {<<"uuid">>, <<"d">>}]),
     InO = jsxd:from_list([{<<"uuid">>, <<"z">>},
@@ -150,7 +150,6 @@ apply_defaults(InP, InD, InO) ->
     InD1 = jsxd:delete(<<"uuid">>, InD),
     jsxd:thread([{merge, InP},
                  {set, <<"autoboot">>, true},
-                 {set, <<"cpu_cap">>, 100},
                  {set, <<"max_swap">>, Swap},
                  {delete, <<"name">>},
                  {set, <<"package">>, <<"p">>},
@@ -171,9 +170,10 @@ apply_defaults_kvm(InP, InD, InO) ->
 apply_defaults_kvm(InP, InD, InO, Cap, N) ->
     In0 = apply_defaults(InP, InD, InO),
     jsxd:thread([{set, <<"cpu_cap">>, Cap},
+                 {delete, <<"image_size">>},
                  {set, <<"disks">>,
                   [[{<<"boot">>, true},
-                    {<<"image_size">>, 0},
+                    {<<"image_size">>, 10},
                     {<<"image_uuid">>, <<"d">>}],
                    [{<<"boot">>, false},
                     {<<"size">>, 10240}]]},
